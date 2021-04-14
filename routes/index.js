5e7c7e91d7367b1f3f218754d9855519ade84db4
Matthew Kindzerske <matthew.k@slalom.com>
2021-04-14 09:17:05 -0400

    single item GET for dynamoDB

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const AWS = require("aws-sdk")
+AWS.config.update({ region: "us-east-1" })
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 const persistence_support = require('../modules/persistence_support');
 
 // '/'
@@ -11,7 +15,15 @@ router.get('/', function(req, res, next) {
 // '/empty_promise'
 router.get('/simple_promise', function(req, res, next) {
   persistence_support.simplePromise().then( function(obj) {
-    res.render('index', { description: 'Index', jay_son: obj });
+    res.render('index', { description: 'Simple Promise', jay_son: obj });
+  });
+});
+
+// '/manufacturer'
+router.get('/manufacturer', function(req, res, next) {
+  var params = { Key: { "name": req.query.name_str }, TableName: "d_bike_manufacturer" };
+  dynamoDB.get(params).promise().then( function(obj) {
+    res.render('index', { description: 'Single DynamoDB Item', jay_son: obj });
   });
 });
 
